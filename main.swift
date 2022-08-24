1b1085a182ded145584abe1bb0277193db8de3d1
FananTamim <110988214+FananTamim@users.noreply.github.com>
2022-08-24 11:06:32 +0300

    Update main.swift

--- main.swift
+++ main.swift
@@ -7,34 +7,30 @@
 
 import Foundation
 
-var flight = Flight()
+
+var server = SeatBooking()
 
 var queue = DispatchQueue.global()
-var firstClassQueue = DispatchQueue.global(qos: .default)
-var econimicQueue = DispatchQueue.global(qos: .utility)
+
+
 for i in 0 ... 24 {
-    queue.sync {
-        flight.bookSeat("\(i)", "Economic", Int.random(in: 41 ..< 100))
+    queue.async {
+        server.bookSeat("\(i)", .economic, Int.random(in: 41 ..< 100))
     }
 }
 for i in 25 ... 50 {
-    queue.sync {
-        flight.bookSeat("\(i)", "First class", Int.random(in: 0 ..< 100))
+    queue.async {
+        server.bookSeat("\(i)", .firstClass, Int.random(in: 0 ..< 100))
+    }
+}
+sleep(10)
+print("-------------------------------------------------------")
+for i in 0 ..< 100 {
+    if let passenger = server.reservedSeats[i] {
+        queue.async {
+            server.changeSeat(passenger, Int.random(in: 0 ..< 100))
+        }
     }
 }
 
-for _ in 0 ... 10 {
-    let passenger = flight.passengers.randomElement()!
-    if passenger.seatType == "First class" {
-        firstClassQueue.sync {
-            print("First class: higher priority")
-            flight.changeSeat(passenger, Int.random(in: 0 ..< 100))
-        }
-    } else {
-        econimicQueue.sync {
-            print("Economic: lower priority")
-            flight.changeSeat(passenger, Int.random(in: 0 ..< 100))
-        }
-    }
-    
-}
+sleep(10)
